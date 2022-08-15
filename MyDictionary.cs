09b08444a7419cebb3a72fae83e1dfc9d534a1d7
Msi <Msi@DESKTOP-IHQ4ULV>
2022-08-15 19:37:23 +0300

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProjectDictionary
+{
+    public class MyDictionary<T,K>
+    {
+        KeyValuePair<T, K>[] items;
+        KeyValuePair<T, K>[] tempItems;
+        public MyDictionary()
+        {
+            items = new KeyValuePair<T, K>[0];
+        }
+        public void Add(T key, K value)
+        {
+            tempItems = items;
+            items = new KeyValuePair<T, K>[items.Length + 1];
+
+            for (int i = 0; i < tempItems.Length; i++)
+            {
+                items[i] = tempItems[i];
+            }
+
+            items[items.Length - 1] = new KeyValuePair<T, K>(key, value);
+        }
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.Value} : {item.Key}");
+            }
+        }
+        public int Count
+        {
+            get { return items.Length; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using MyProjectDictionary;
+
+MyDictionary<int, string> plakaveSehirler = new MyDictionary<int, string>();
+plakaveSehirler.Add(9, "Aydın");
+plakaveSehirler.Add(44, "Malatya");
+plakaveSehirler.Add(35, "İzmir");
+
+Console.WriteLine(plakaveSehirler.Count);
+Console.WriteLine();
+plakaveSehirler.ShowList();
